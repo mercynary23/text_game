9faf9704bab2fb9c91c98b71b313c1552e168e20
mercynary23 <mercynary23@gmail.com>
2022-10-25 00:06:00 +0100

    My world is growing

--- greed_island/game.py
+++ greed_island/game.py
@@ -1,3 +1,18 @@
-action_input = input("Action:\n")
+print("Welcome to Greed Island :D! Are you ready to die?")
+
+action_input = input("Where would you like to go?\n")
 print(action_input)
+
+if action_input == 'n':
+    print("Go North!")
+elif action_input == 'e':
+    print("Go East!")
+elif action_input == 'w':
+    print("Go West!")
+elif action_input == 's':
+    print("Go South!")
+else:
+    print("Invalid action!")
+
+
 
