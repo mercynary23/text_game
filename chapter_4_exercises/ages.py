843390e0be1ec68cc5a3e4d70e2d9068ac7be88b
mercynary23 <mercynary23@gmail.com>
2022-10-25 02:21:00 +0100

    Working with conditionals

---
+++ chapter_4_exercises/ages.py
@@ -0,0 +1,13 @@
+user_age = input("How old are you Sailor?\n")
+int_user_age = int(user_age)
+
+if int_user_age < 13:
+    print("You aren't even a teenager yet mi'lad! Back to school for you!")
+elif int_user_age < 18:
+    print("A few more years lad; you'll be on the waters soon enough")
+elif int_user_age > 18 and int_user_age < 30:
+    print("Well shiver me timbers sonny! Are you ready to die? Harr Harr Harr!")
+else:
+    print("You're too old; stay home, you've done enough")
+
+
