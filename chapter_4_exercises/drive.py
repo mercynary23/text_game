e6b6545d5e3a93d64a2393c6b41d1000bfe84d87
mercynary23 <mercynary23@gmail.com>
2022-10-29 20:21:59 +0100

    Working with conditionals

---
+++ chapter_4_exercises/drive.py
@@ -0,0 +1,10 @@
+hinge_date = input("How hot is your date tonight?\n")
+
+if hinge_date == 'Sexy':
+    print("You lucky devil!")
+elif hinge_date == 'Hot':
+    print("I think you're in for a fun night XD ")
+else:
+    print("This was a colossal waste of time")
+
+
