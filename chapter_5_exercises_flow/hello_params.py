65ee48de54b1dc94557232d7c5808d9855b29fd1
mercynary23 <mercynary23@gmail.com>
2022-11-22 19:39:33 +0000

    More

--- chapter_5_exercises_flow/hello_params.py
+++ chapter_5_exercises_flow/hello_params.py
@@ -5,7 +5,7 @@ def avatar_conditional_access(first_name, last_name, age):
     if age < 18:
         age_lapse = int(18 - age) 
         print(f"Hi {first_name}! Appears you're too young for this ride; we'll see you in {age_lapse} years :D")
-    elif 18 < age and age < 30:
+    elif 18 <= age and age < 30:
         print(f"Right this way {first_name}; we've been expecting you")
     else:
         print("Relax Mr {last_name}")
@@ -22,3 +22,5 @@ def say_hello(name):
     print("Hello, " + name)
 
 say_hello("Una")
+say_hello("James")
+
