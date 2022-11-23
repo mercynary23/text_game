e6e6245ebdb5011b6c996b9e735f444eb64523dd
mercynary23 <mercynary23@gmail.com>
2022-11-23 15:35:37 +0000

    Clean Run

--- greed_island/game_iii.py
+++ greed_island/game_iii.py
@@ -1,8 +1,12 @@
 #Write two functions
 #Function_1 will host the main game cascade
 #Function_2 will absorb user input concerning direction 
+#Add an inventory into the play_game func; must contain at least 3 items  
+#Add an 'i' based conditional which visualises the inventory 
+#Update if/elif to work with arrays
 
 def play_game():
+    user_inventory = ['Sword', 'Shield', 'Gold(5)', 'Wine', 'Mutton'] 
     print("Welcome to Greed Island - are you ready to Die? XD")
     #ui_name = input("What is your name, Hunter?\n")
     ui_name = user_name_prompt()
@@ -16,6 +20,11 @@ def play_game():
         print("The night is dark and full of terrors in the South")
     elif ui_dir == 'W' or ui_dir == 'w':
         print(f"Are you sure this is wise {ui_name}?")
+    elif ui_dir in ['I', 'i']:
+        print("You've chosen to view your inventory:\n")
+        for item in user_inventory:
+            print(f"You have: {item} ")
+        print("These are all of the items in your inventory")
     else:
         print("Are you afraid?")
 
