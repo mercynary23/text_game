a45d02d720734c64a7726556a3559c90155ceb95
mercynary23 <mercynary23@gmail.com>
2022-11-23 15:26:42 +0000

    Run

---
+++ chapter_6_exercises/arr_ii.py
@@ -0,0 +1,21 @@
+arr_nfl = ['Julio', 'Lynch', 'Vick', 'Willis', 'Rice']
+print(arr_nfl)
+
+hof_ceremony_status = 'On' 
+
+while hof_ceremony_status == 'On':
+    hof_ceremony_status = input("Do you want to keep watching?\n")
+
+print("See you next year")
+
+arr_soccer = ['Beckham', 'Benzema', 'Eto', 'Sadio', 'Pato']
+print(arr_soccer)
+
+iterator_soccer = 0 
+
+while iterator_soccer < len(arr_soccer):
+    print(f"Wearing number {str(iterator_soccer + 1)} we have {arr_soccer[iterator_soccer]}")
+    iterator_soccer = iterator_soccer + 1
+
+print("Thanks for playing!")
+
