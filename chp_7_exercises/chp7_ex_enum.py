f6cb992920c0368297e6c8e3d262a55685b8a808
mercynary23 <mercynary23@gmail.com>
2022-11-23 22:28:01 +0000

    Run

---
+++ chp_7_exercises/chp7_ex_enum.py
@@ -0,0 +1,8 @@
+arr_nba = ['MJ', 'Shaq', 'Giannis', 'Magic', 'Duncan'] 
+
+#arr returned essentially functions as a 2D array 
+arr_enum = list(enumerate(arr_nba))
+
+print(arr_enum)
+print(str(arr_enum[2][0]))
+
---
+++ chp_7_exercises/chp7_ex_iii.py
@@ -0,0 +1,6 @@
+first_name, last_name = ('Barack', 'Obama')
+month, day, year = (10, 22, 2015) 
+dna_aminos = ('A', 'T', 'C', 'G')
+
+print(dna_aminos)
+
